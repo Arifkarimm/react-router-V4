614090272f4113ed3a7da59322e998c272944a89
Fazlul <fkarif.cse@gmail.com>
2019-10-10 00:13:26 +0200

    basic router configure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Nav from './components/Nav';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
+    <BrowserRouter>
+      <div className="App-header">
+        <Nav />
+      </div>
+
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
